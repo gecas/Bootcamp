309fa249096cf9d6f688feb320572d29a60058d6
Marius <geciauskasmarius@gmail.com>
2016-07-19 15:11:26 +0300

    first commit

---
+++ Jsonable.php
@@ -0,0 +1,13 @@
+<?php
+
+interface CastsToJson{
+	public function toJson();
+}
+
+class User implements CastsToJson{
+
+}
+
+class Collection implements CastsToJson{
+	
+}
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once('Interface.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Interfaces</title>
+</head>
+<body></body>
+</html>
